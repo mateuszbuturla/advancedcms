5621955efb1e0fa9badb145ef61d62bb76d589ba
mateuszbuturla <2002mateuszbuturla@gmail.com>
2020-04-11 19:23:30 +0200

    create get home page api

--- server/controllers/homePageController.js
+++ server/controllers/homePageController.js
@@ -1,6 +1,21 @@
 const homePageModel = require('../models/homePageModel');
 const mongoose = require('mongoose');
 
+exports.getHomePage = async (req, res) => {
+    const findHomePage = await homePageModel.find({});
+    if (findHomePage.length > 0) {
+        res.status(200).json({ homePage: findHomePage[0] })
+    }
+    else {
+        homePageModel.create({ _id: mongoose.Types.ObjectId(), elements: [] }, (err) => {
+            if (err)
+                return res.status(500);
+
+            res.status(200).json({ homePage: findHomePage[0] });
+        })
+    }
+}
+
 exports.editHomePage = async (req, res) => {
     const content = req.body.content;
     const findHomePage = await homePageModel.find({});
