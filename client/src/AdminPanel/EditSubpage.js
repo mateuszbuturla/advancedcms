d5b57c79f5a575f0ec249d58fed440c815159c08
mateuszbuturla <2002mateuszbuturla@gmail.com>
2020-04-12 23:02:32 +0200

    create edit subpage form

---
+++ client/src/AdminPanel/EditSubpage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import axios from 'axios';
+
+class EditHomePage extends React.Component {
+
+
+    render() {
+
+        return (
+            <div>
+                <h2>Edit Subpage</h2>
+                <input type="text" placeholder="Subpage name" />
+
+            </div>
+        );
+    }
+}
+
+export default EditHomePage;
--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -3,6 +3,7 @@ import { NavLink, Switch, Route } from 'react-router-dom';
 import { getUser, removeUserSession } from './Utils/Common';
 
 import EditHomePage from './AdminPanel/EditHomePage';
+import CreateSubpage from './AdminPanel/EditSubpage';
 
 function Dashboard(props) {
   const user = getUser();
@@ -16,10 +17,11 @@ function Dashboard(props) {
     <div>
       <p>Advanced CMS Dashboard</p>
       <NavLink to="/dashboard/edithomepage">Edit Home Page</NavLink>
+      <NavLink to="/dashboard/createsubpage">Create subpage</NavLink>
       <input type="button" onClick={handleLogout} value="Logout" />
-
       <Switch>
         <Route exact path="/dashboard/edithomepage" component={EditHomePage} />
+        <Route exact path="/dashboard/createsubpage" component={CreateSubpage} />
       </Switch>
     </div>
   );
