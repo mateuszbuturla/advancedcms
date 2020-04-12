e60e7d4129f951ab52f1e69a754fd923db97b16b
mateuszbuturla <2002mateuszbuturla@gmail.com>
2020-04-12 11:50:56 +0200

    get subpages and show then in dashboard menu

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,30 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavLink, Switch, Route } from 'react-router-dom';
 import { getUser, removeUserSession } from './Utils/Common';
+import axios from 'axios';
 
 import EditHomePage from './AdminPanel/EditHomePage';
 import CreateSubpage from './AdminPanel/EditSubpage';
 
 function Dashboard(props) {
-  const user = getUser();
+    const [subpages, setSubpages] = useState([]);
 
-  const handleLogout = () => {
-    removeUserSession();
-    props.history.push('/login');
-  }
+    const user = getUser();
 
-  return (
-    <div>
-      <p>Advanced CMS Dashboard</p>
-      <NavLink to="/dashboard/edithomepage">Edit Home Page</NavLink>
-      <NavLink to="/dashboard/createsubpage">Create subpage</NavLink>
-      <input type="button" onClick={handleLogout} value="Logout" />
-      <Switch>
-        <Route exact path="/dashboard/edithomepage" component={EditHomePage} />
-        <Route exact path="/dashboard/createsubpage" component={CreateSubpage} />
-      </Switch>
-    </div>
-  );
+    useEffect(() => {
+        axios.get('http://localhost:4000/api/getallsubpages')
+            .then(response => {
+                setSubpages(response.data.subpages)
+            }).catch(error => {
+                console.log('error')
+            });
+    }, [])
+
+    const handleLogout = () => {
+        removeUserSession();
+        props.history.push('/login');
+    }
+
+    const subpagesLinks = subpages.map((element, index) =>
+        <>
+            <NavLink to={`/dashboard/editsubpage/${element._id}`}>{element.name}</NavLink><br />
+        </>)
+
+    return (
+        <div>
+            <p>Advanced CMS Dashboard</p>
+            <NavLink to="/dashboard/edithomepage">Edit Home Page</NavLink><br />
+            <NavLink to="/dashboard/createsubpage">Create subpage</NavLink><br />
+            <a>Subpages</a><br />
+            {subpagesLinks}
+            <input type="button" onClick={handleLogout} value="Logout" />
+            <Switch>
+                <Route exact path="/dashboard/edithomepage" component={EditHomePage} />
+                <Route exact path="/dashboard/createsubpage" component={CreateSubpage} />
+            </Switch>
+        </div>
+    );
 }
 
 export default Dashboard;
