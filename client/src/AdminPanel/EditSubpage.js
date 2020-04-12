df00e710697fcb5b13970678a844ed44ff4d55ff
mateuszbuturla <2002mateuszbuturla@gmail.com>
2020-04-12 23:05:32 +0200

    edit subpae create add element system

--- client/src/AdminPanel/EditSubpage.js
+++ client/src/AdminPanel/EditSubpage.js
@@ -1,16 +1,47 @@
 import React from 'react';
 import axios from 'axios';
 
+import EditText from '../Components/AdminPanel/EditText';
+
 class EditHomePage extends React.Component {
 
+    state = {
+        content: [],
+    }
+
+    addText() {
+        let newContent = this.state.content;
+        newContent.push({ type: 'text', text: '' })
+        this.setState({ content: newContent });
+    }
+
+    handleChangeText(e) {
+        let newContent = this.state.content;
+        newContent[e.target.id].text = e.target.value;
+        this.setState({ content: newContent })
+    }
 
     render() {
+        const { content } = this.state;
+
+        const elements = content.map((element, index) =>
+            <EditText
+                key={index}
+                handleChangeText={this.handleChangeText.bind(this)}
+                value={element.text}
+                id={index}
+            />)
 
         return (
             <div>
                 <h2>Edit Subpage</h2>
                 <input type="text" placeholder="Subpage name" />
-
+                <hr />
+                {elements}
+                <hr />
+                <button onClick={this.addText.bind(this)}>
+                    Add Text
+                </button>
             </div>
         );
     }
