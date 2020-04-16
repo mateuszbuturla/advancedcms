8788ba8455efbf0329c5c3e3a579803e8ee135ff
mateuszbuturla <2002mateuszbuturla@gmail.com>
2020-04-16 13:53:18 +0200

    edit main nav get subpages and allow to select it from select box

--- client/src/Components/AdminPanel/EditMainNavLink.js
+++ client/src/Components/AdminPanel/EditMainNavLink.js
@@ -1,11 +1,32 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function EditMainNavLink(props) {
+
+    const [subpages, setSubpages] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:4000/api/getallsubpages')
+            .then(response => {
+                setSubpages(response.data.subpages)
+            }).catch(error => {
+                console.log('error')
+            });
+    }, [])
+
+    const subpagesOptions = subpages.map((subpage, index) => {
+        return (
+            <option key={index} value={subpage.name}>
+                {subpage.name}
+            </option>
+        )
+    })
 
     return (
         <div>
             <select onChange={props.handleChangeText} value={props.value} id={props.id} name="subpageSelect">
-
+                <option value="homepage">Home Page</option>
+                {subpagesOptions}
             </select>
             <button id={props.id} data-direction='up' onClick={props.handleChangeElementPosition}>UP</button>
             <button id={props.id} data-direction='down' onClick={props.handleChangeElementPosition}>DOWN</button>
