ffc2d376aa854eb71c8f94f3658becd6574e5aae
mateuszbuturla <2002mateuszbuturla@gmail.com>
2020-05-03 12:16:59 +0200

    add styledComponents, create styledComponentsConfig and add style reset

---
+++ client/src/config/styledComponentsConfig.js
@@ -0,0 +1,20 @@
+export const styledComponentsConfig = {
+    bgColor: '#ffffff',
+    mainColor: '#000000',
+    themeColor: '#2b8aba',
+    mainContainerSize: {
+        size: '90vw',
+        maxSize: '1400px'
+    },
+    headerFontSize: {
+        small: '25px',
+        medium: '35px',
+        big: '45px'
+    },
+    textFontSize: {
+        small: '15px',
+        medium: '17px',
+        big: '19px'
+    },
+    transition: '.3s'
+}
