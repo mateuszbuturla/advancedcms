3db23a222f05a7f17b809317a273b5d733afeb36
mateuszbuturla <2002mateuszbuturla@gmail.com>
2020-05-15 00:35:02 +0200

    create pageConfig

---
+++ client/src/config/pageConfig.js
@@ -0,0 +1,46 @@
+module.exports = {
+    homePage: {
+        editButtons: {
+            addHeader: true,
+            addText: true,
+            addLink: true,
+            addList: true,
+            addMainNavLink: false,
+            save: true,
+            remove: false,
+        },
+    },
+    createSubpage: {
+        editButtons: {
+            addHeader: true,
+            addText: true,
+            addLink: true,
+            addList: true,
+            addMainNavLink: false,
+            save: true,
+            remove: false,
+        },
+    },
+    editSubpage: {
+        editButtons: {
+            addHeader: true,
+            addText: true,
+            addLink: true,
+            addList: true,
+            addMainNavLink: false,
+            save: true,
+            remove: true,
+        },
+    },
+    footer: {
+        editButtons: {
+            addHeader: false,
+            addText: true,
+            addLink: true,
+            addList: true,
+            addMainNavLink: false,
+            save: true,
+            remove: false,
+        },
+    },
+}
